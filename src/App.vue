bba32fd273c9caf0df974bc41c039b45ceca42cc
fracs31 <pist.fracs95@gmail.com>
2023-03-01 14:05:53 +0100

    IMPORT AppHeader in App

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,20 @@
+<!-- JavaScript -->
 <script>
+import AppHeader from "./components/AppHeader.vue";
+
+export default {
+  components: {
+    AppHeader,
+  }
+}
+
 </script>
 
+<!-- Template -->
 <template>
-  
+  <AppHeader></AppHeader>
 </template>
 
+<!-- CSS -->
 <style scoped>
 </style>

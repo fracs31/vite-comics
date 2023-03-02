4e87184544952923bd03bdfa366ff4161e214d35
fracs31 <pist.fracs95@gmail.com>
2023-03-02 10:42:14 +0100

    CHANGE Discover in a component

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,5 +1,15 @@
 <!-- JavaScript -->
 <script>
+import Discover from "./Discover.vue"; //importo "Discover.vue"
+
+//Esporto
+export default {
+    //Componenti
+    components: {
+        Discover, //Discover
+    }
+}
+
 </script>
 
 <!-- Template -->
@@ -15,74 +25,7 @@
             </div>
         </div>
         <!-- Discover -->
-        <div class="discover">
-            <!-- Container -->
-            <div class="container">
-                <!-- Lista -->
-                <ul class="discover__list">
-                    <!-- Elemento della lista -->
-                    <li class="discover__list-item">
-                        <!-- Immagine -->
-                        <div class="discover__list-item--img">
-                            <!-- Immagine -->
-                            <img class="discover__list-item--img__img" src="../assets/img/buy-comics-digital-comics.png" alt="Digital Comics">
-                        </div>
-                        <!-- Descrizione -->
-                        <div class="discover__list-item--description">
-                            Digital Comics
-                        </div>
-                    </li>
-                    <!-- Elemento della lista -->
-                    <li class="discover__list-item">
-                        <!-- Immagine -->
-                        <div class="discover__list-item--img">
-                            <!-- Immagine -->
-                            <img class="discover__list-item--img__img" src="../assets/img/buy-comics-merchandise.png" alt="DC Merchandise">
-                        </div>
-                        <!-- Descrizione -->
-                        <div class="discover__list-item--description">
-                            DC Merchandise
-                        </div>
-                    </li>
-                    <!-- Elemento della lista -->
-                    <li class="discover__list-item">
-                        <!-- Immagine -->
-                        <div class="discover__list-item--img">
-                            <!-- Immagine -->
-                            <img class="discover__list-item--img__img" src="../assets/img/buy-comics-subscriptions.png" alt="Subscription">
-                        </div>
-                        <!-- Descrizione -->
-                        <div class="discover__list-item--description">
-                            Subscription
-                        </div>
-                    </li>
-                    <!-- Elemento della lista -->
-                    <li class="discover__list-item">
-                        <!-- Immagine -->
-                        <div class="discover__list-item--img">
-                            <!-- Immagine -->
-                            <img class="discover__list-item--img__img" src="../assets/img/buy-comics-shop-locator.png" alt="Comic Shop Locator">
-                        </div>
-                        <!-- Descrizione -->
-                        <div class="discover__list-item--description">
-                            Comic Shop Locator
-                        </div>
-                    </li>
-                    <!-- Elemento della lista -->
-                    <li class="discover__list-item">
-                        <!-- Immagine -->
-                        <div class="discover__list-item--img">
-                            <!-- Immagine -->
-                            <img class="discover__list-item--img__img" src="../assets/img/buy-dc-power-visa.svg" alt="DC Power Visa">
-                        </div>
-                        <!-- Descrizione -->
-                        <div class="discover__list-item--description">
-                            DC Power Visa
-                        </div>
-                    </li>
-                </ul>
-            </div>
-        </div>
+        <Discover></Discover>
     </main>
 </template>
 
@@ -92,36 +35,6 @@
     .content {
         background-color: var(--black);
         padding: 45px 0px;
-        color: white;
-    }
-    /* Discover */
-    .discover {
-        background-color: var(--blue);
-        padding: 45px 0px;
-        position: relative;
-        z-index: 1;
-    }
-    /* Lista di Discover */
-    .discover__list {
-        display: flex;
-        justify-content: space-around;
-        gap: 50px;
-    }
-    /* Elemento della lista di Discover */
-    .discover__list-item {
-        display: flex;
-        align-items: center;
-        gap: 10px;
-    }
-    /* Immagine dell'elemento della lista di Discover */
-    .discover__list-item--img__img {
-        width: 50px;
-        aspect-ratio: 1/1;
-        object-fit: contain;
-    }
-    /* Descrizione dell'elemento della lista di Discover */
-    .discover__list-item--description {
-        text-transform: uppercase;
         color: white;
     }
 </style>
---
+++ src/components/Discover.vue
@@ -0,0 +1,110 @@
+<!-- JavaScript -->
+<script>
+</script>
+
+<!-- Template -->
+<template>
+    <!-- Discover -->
+    <div class="discover">
+        <!-- Container -->
+        <div class="container">
+            <!-- Lista -->
+            <ul class="discover__list">
+                <!-- Elemento della lista -->
+                <li class="discover__list-item">
+                    <!-- Immagine -->
+                    <div class="discover__list-item--img">
+                        <!-- Immagine -->
+                        <img class="discover__list-item--img__img" src="../assets/img/buy-comics-digital-comics.png" alt="Digital Comics">
+                    </div>
+                    <!-- Descrizione -->
+                    <div class="discover__list-item--description">
+                        Digital Comics
+                    </div>
+                </li>
+                <!-- Elemento della lista -->
+                <li class="discover__list-item">
+                    <!-- Immagine -->
+                    <div class="discover__list-item--img">
+                        <!-- Immagine -->
+                        <img class="discover__list-item--img__img" src="../assets/img/buy-comics-merchandise.png" alt="DC Merchandise">
+                    </div>
+                    <!-- Descrizione -->
+                    <div class="discover__list-item--description">
+                        DC Merchandise
+                    </div>
+                </li>
+                <!-- Elemento della lista -->
+                <li class="discover__list-item">
+                    <!-- Immagine -->
+                    <div class="discover__list-item--img">
+                        <!-- Immagine -->
+                        <img class="discover__list-item--img__img" src="../assets/img/buy-comics-subscriptions.png" alt="Subscription">
+                    </div>
+                    <!-- Descrizione -->
+                    <div class="discover__list-item--description">
+                        Subscription
+                    </div>
+                </li>
+                <!-- Elemento della lista -->
+                <li class="discover__list-item">
+                    <!-- Immagine -->
+                    <div class="discover__list-item--img">
+                        <!-- Immagine -->
+                        <img class="discover__list-item--img__img" src="../assets/img/buy-comics-shop-locator.png" alt="Comic Shop Locator">
+                    </div>
+                    <!-- Descrizione -->
+                    <div class="discover__list-item--description">
+                        Comic Shop Locator
+                    </div>
+                </li>
+                <!-- Elemento della lista -->
+                <li class="discover__list-item">
+                    <!-- Immagine -->
+                    <div class="discover__list-item--img">
+                        <!-- Immagine -->
+                        <img class="discover__list-item--img__img" src="../assets/img/buy-dc-power-visa.svg" alt="DC Power Visa">
+                    </div>
+                    <!-- Descrizione -->
+                    <div class="discover__list-item--description">
+                        DC Power Visa
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<!-- CSS -->
+<style scoped>
+    /* Discover */
+    .discover {
+        background-color: var(--blue);
+        padding: 45px 0px;
+        position: relative;
+        z-index: 1;
+    }
+    /* Lista di Discover */
+    .discover__list {
+        display: flex;
+        justify-content: space-around;
+        gap: 50px;
+    }
+    /* Elemento della lista di Discover */
+    .discover__list-item {
+        display: flex;
+        align-items: center;
+        gap: 10px;
+    }
+    /* Immagine dell'elemento della lista di Discover */
+    .discover__list-item--img__img {
+        width: 50px;
+        aspect-ratio: 1/1;
+        object-fit: contain;
+    }
+    /* Descrizione dell'elemento della lista di Discover */
+    .discover__list-item--description {
+        text-transform: uppercase;
+        color: white;
+    }
+</style>

f04fb80a4978f1044a47cda59e4c58d7b10d987c
fracs31 <pist.fracs95@gmail.com>
2023-03-02 14:43:43 +0100

    ADD card props

---
+++ src/components/Card.vue
@@ -0,0 +1,53 @@
+<!-- JavaScript -->
+<script>
+
+//Esporto
+export default {
+    //Props
+    props: {
+        thumb: String, //immagine
+        series: String, //nome della serie
+    }
+}
+
+</script>
+
+<!-- Template -->
+<template>
+    <!-- Carta -->
+    <div class="card">
+        <!-- Immagine della carta-->
+        <div class="card__img">
+            <!-- Immagine -->
+            <img class="card__img--img" v-bind:src="thumb" v-bind:alt="series">
+        </div>
+        <!-- Titolo della carta -->
+        <div class="card__title">
+            <!-- Titolo -->
+            <h3 class="card__title--title">
+                {{ series }}
+            </h3>
+        </div>
+    </div>
+</template>
+
+<!-- CSS -->
+<style scoped>
+    /* Immagine della carta */
+    .card__img {
+        margin-bottom: 15px;
+    }
+    /* Immagine usata della carta */
+    .card__img--img {
+        width: 185px;
+        aspect-ratio: 1/1;
+        object-fit: cover;
+        object-position: top;
+    }
+    /* Titolo usato della carta */
+    .card__title--title {
+        text-transform: uppercase;
+        font-weight: normal;
+        font-size: 16px;
+    }
+</style>
--- src/components/Content.vue
+++ src/components/Content.vue
@@ -24,21 +24,6 @@
                 <ul class="current-series__list">
                     <!-- Elemento della lista -->
                     <li class="current-series__list-item">
-                        <!-- Carta -->
-                        <div class="card">
-                            <!-- Immagine della carta-->
-                            <div class="card__img">
-                                <!-- Immagine -->
-                                <img class="card__img--img" src="../assets/img/prova.jpg" alt="">
-                            </div>
-                            <!-- Titolo della carta -->
-                            <div class="card__title">
-                                <!-- Titolo -->
-                                <h3 class="card__title--title">
-                                    Action Comics
-                                </h3>
-                            </div>
-                        </div>
                     </li>
                 </ul>
                 <!-- Bottone -->
@@ -89,23 +74,6 @@
         display: flex;
         flex-wrap: wrap;
         gap: 50px 25px;
-    }
-    /* Immagine della carta */
-    .card__img {
-        margin-bottom: 15px;
-    }
-    /* Immagine usata della carta */
-    .card__img--img {
-        width: 185px;
-        aspect-ratio: 1/1;
-        object-fit: cover;
-        object-position: top;
-    }
-    /* Titolo usato della carta */
-    .card__title--title {
-        text-transform: uppercase;
-        font-weight: normal;
-        font-size: 16px;
     }
     /* Bottone */
     .current-series__button {
